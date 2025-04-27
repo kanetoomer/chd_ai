5bfc94142fb41ec1687e277280f6bcdd1cf02f1a
Kane Toomer <kanetoomer@yahoo.com>
2025-04-27 16:22:58 -0700

    updated API_URL

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://chd-ai-backend.onrender.com/";
+const API_URL = "https://chd-ai-backend.onrender.com";
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
