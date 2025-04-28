c4f39659c80f632a2766116ba256c9390252495d
Kane Toomer <kanetoomer@yahoo.com>
2025-04-27 17:42:19 -0700

    server updates

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 
 // Import configurations
 const connectDB = require("./config/db");
-const corsOptions = require("./config/corsConfig");
 const errorHandler = require("./middleware/errorHandler");
 
 // Initialize Express app
@@ -18,8 +17,20 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
+// Allow frontend
+const allowedOrigins = [
+  "http://localhost:5173", // dev
+  "https://chd-ai.onrender.com", // deployed frontend
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 // Apply middleware
-app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
