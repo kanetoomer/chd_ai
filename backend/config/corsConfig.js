88e7c9e52ea48cb683fae3af202bf40af37ca8fe
Kane Toomer <kanetoomer@yahoo.com>
2025-04-27 17:24:25 -0700

    server updates

--- backend/config/corsConfig.js
+++ backend/config/corsConfig.js
@@ -1,15 +1,21 @@
-const allowedOrigins = ["http://localhost:5173", "https://chd-ai.onrender.com"];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+  origin: function (origin, callback) {
+    // Allow requests from the frontend or API development tools
+    const allowedOrigins = [
+      "http://localhost:5173", // For local dev
+      "https://chd-ai.onrender.com", // Your frontend production URL
+    ];
+
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+  credentials: true,
+  maxAge: 86400, // 24 hours
 };
 
 module.exports = corsOptions;
